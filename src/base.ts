0d215c0d801b431b0da0d3e919489de6201b45cb
xiaoYown <qzy09101018@sina.com>
2022-03-29 17:22:34 +0800

    feat: Init sidebar template

---
+++ src/base.ts
@@ -0,0 +1,9 @@
+class SidebarItemBase {
+  public onMount = (): void => {};
+
+  public onUnmount = (): void => {};
+
+  public onError = (): void => {};
+}
+
+export default SidebarItemBase;
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import SidebarItemBase from './base';
+
+import styles from './main.module.css';
+
+const a = styles.test;
+
+class SidebarItem extends SidebarItemBase {}
+
+export default SidebarItem;
